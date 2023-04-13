76e8467b1b5349f4c6ea5cb8fea33305ebd38037
amanpatel23 <ap289503@gmail.com>
2023-04-13 22:05:13 +0530

    added post section and showed posts on the home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,20 @@
-module.exports.home = function(req, res) {
-    console.log(req.cookies);
-    res.cookie('user_id', 1);
-    return res.render('home', {
-        title: "Home"
-    })
+
+const Post = require('../models/post');
+module.exports.home = async function(req, res) {
+    // console.log(req.cookies);
+    // res.cookie('user_id', 1);
+
+    try {
+
+        // populate the user of each post
+        const posts = await Post.find({}).populate('user').exec();
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        })
+    }
+    catch (err) {
+        console.log('error while retrieving the posts: ', err);
+    }
+    
 }
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const home_controller = require('../controllers/home_controller');
 
 router.get('/', home_controller.home);
 router.use('/users', require('./users'));
+router.use('/post', require('./posts'));
 
 // for any further routes, access from here
 // router.use('/routerName', require('./routerFile'));
