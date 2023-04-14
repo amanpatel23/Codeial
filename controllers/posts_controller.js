ed9f7cfaf6e81dbe2a55f98595a2fc7aa17242f2
amanpatel23 <ap289503@gmail.com>
2023-04-14 22:01:56 +0530

    deleting a Post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res) {
     try {
@@ -10,5 +11,25 @@ module.exports.create = async function(req, res) {
     }catch(err) {
         console.log('error in creating a post ', err);
         return;
+    }
+}
+
+module.exports.destroy = async function(req, res) {
+    try {
+        const post = await Post.findById(req.params.id);
+        // .id means converting the object id into string
+        if (post.user == req.user.id) {
+            await Post.deleteOne({_id: post._id});
+            try {
+                await Comment.deleteMany({post: req.params.id});
+            }
+            catch (err) {
+                console.log('error in deleting the comments ', err);
+            }  
+        }
+        return res.redirect('back');
+    }
+    catch (err) {
+        console.log('err in finding the post ', err);
     }
 }
