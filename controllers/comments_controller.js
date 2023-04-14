3ec2a28f0812a962b62eb7e2ae3505eb73da557d
amanpatel23 <ap289503@gmail.com>
2023-04-14 22:31:29 +0530

    deleting a commment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -23,3 +23,25 @@ module.exports.create = async function(req, res) {
         console.log(err);
     }
 }
+
+module.exports.destroy = async function (req, res) {
+    try {
+        const comment = await Comment.findById(req.params.id);
+        if (comment.user == req.user.id) {
+            let postId = comment.post;
+            await comment.deleteOne({_id: comment._id});
+            try {
+                await Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}});
+                return res.redirect('back');
+            }
+            catch {
+                console.log('error while deleting the comment from the comments array of the post ', err);
+            }
+        } else {
+            return res.redirect('back'); 
+        }
+    }
+    catch (err) {
+        console.log('error in finding the comment ', err);
+    }
+}
